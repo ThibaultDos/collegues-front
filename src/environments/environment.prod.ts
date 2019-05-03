bde8639a27fce37bd307ed6fdc6830d823b90b24
Thibault <thibault.dos-anjos@lilo.org>
2019-05-03 10:03:09 +0200

    rectification URL backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   
   production: true,
 
-  backendURL : 'https://dashboard.heroku.com/apps/dosanjos-collegues-api/collegues'
+  backendURL : 'https://dashboard.heroku.com/apps/dosanjos-collegues-api'
 };
